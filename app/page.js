ee6654b9d58feda37c6e6ad848e2f5f95cc5f638
Rushikesh chandanshiv <chandanshiv406@gmail.com>
2026-03-03 21:36:40 +0530

    fix: update greeting text in Home component

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default async function Home() {
   // const post = await prisma.post.findMany()
   return (
     <div className="container min-h-screen ">
-    <h1>Hello</h1>
+    <h1>Hello hw</h1>
     </div>
   );
 }
